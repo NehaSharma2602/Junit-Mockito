e230b89416e5dda436c0fc4825ae43848d9da6e0
Neha Sharma <neha2sharma15@gmail.com>
2026-08-30 18:48:09 +0530

    Learning basic of junit

---
+++ src/main/java/com/neha/Calculator.java
@@ -0,0 +1,20 @@
+package com.neha;
+
+public class Calculator {
+
+	public int add (int a, int b) {
+		return a+b;
+	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public float divide(int a, int b) {
+		return (float) a/b;
+	}
+}
---
+++ src/test/java/com/neha/CalculatorTest.java
@@ -0,0 +1,67 @@
+package com.neha;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+	/*@Test
+	public void testAdd() {
+		Calculator cu = new Calculator();
+		int actual = cu.add(3, 4);
+		assertEquals(7, actual);
+		
+	}*/
+	
+	@Test
+	public void testAdd() {
+		Calculator cu = new Calculator();
+		int actual = cu.add(-2, -3);
+		assertEquals(-5, actual);
+		
+	}
+	
+	/*@Test
+	public void testSub(){
+		Calculator cu = new Calculator();
+		int actual = cu.sub(5, 4);
+		assertEquals(1, actual);
+	}*/
+	
+	@Test
+	public void testSub(){
+		Calculator cu = new Calculator();
+		int actual = cu.sub(5, 10);
+		assertEquals(-5, actual);
+	}
+	
+	/*@Test
+	public void testMul() {
+		Calculator cu = new Calculator();
+		int actual = cu.mul(3, 4);
+		assertEquals(12, actual);
+	}*/
+	
+	@Test
+	public void testMul() {
+		Calculator cu = new Calculator();
+		int actual = cu.mul(0, 100);
+		assertEquals(0, actual);
+	}
+	
+	/*@Test
+	public void testDivide() {
+		Calculator cu = new Calculator();
+		int actual = cu.divide(30, 3);
+		assertEquals(10, actual);
+	}*/
+	
+	@Test
+	public void testDivide() {
+		Calculator cu = new Calculator();
+		float actual = cu.divide(9, 2);
+		assertEquals(4.5F, actual);
+	}
+}
