138f77b2bf887ea8c05a592126c33837c2e8ab5d
Neha Sharma <neha2sharma15@gmail.com>
2026-09-02 09:12:32 +0530

    Created a class for learing assertions

---
+++ src/main/java/com/neha/Assertions.java
@@ -0,0 +1,16 @@
+package com.neha;
+
+public class Assertions {
+
+	public boolean isEven(int num) {
+		return num % 2 == 0;
+	}
+	
+	public String getMessage() {
+		return "welcome";
+	}
+	
+	public String getName() {
+		return null;
+	}
+}
